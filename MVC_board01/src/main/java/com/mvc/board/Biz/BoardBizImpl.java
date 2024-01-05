58391d07f1cdceb87c0f766ef63a78db14a32b6b
computer9302 <computer9302@gmail.com>
2024-01-05 17:58:51 +0900

    Spring CRUD(초기설정) - Spring Legacy Project 생성으로 진행

---
+++ MVC_board01/src/main/java/com/mvc/board/Biz/BoardBizImpl.java
@@ -0,0 +1,23 @@
+package com.mvc.board.Biz;
+
+import java.util.List;
+
+import com.mvc.board.Dto.BoardDto;
+
+public class BoardBizImpl implements BoardBiz {
+
+	@Override
+	public List<BoardDto> selectList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDto selectOne(int bd_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
---
+++ MVC_board01/src/main/java/com/mvc/board/Dao/BoardDaoImpl.java
@@ -0,0 +1,23 @@
+package com.mvc.board.Dao;
+
+import java.util.List;
+
+import com.mvc.board.Dto.BoardDto;
+
+public class BoardDaoImpl implements BoardDao {
+
+	@Override
+	public List<BoardDto> seleList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDto selectOne(int bd_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
